07d045ec19e041135f57af8e18d0f3dcd94c60f0
nisha-shaikh <35392508+nisha-shaikh@users.noreply.github.com>
2020-06-29 03:40:45 +0500

    Added Reddit API

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -3,14 +3,39 @@ import json
 from django.core import serializers
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+
 from newsapi import NewsApiClient
-from collections import ChainMap
+import praw
 
 # Create your views here.
 
+#TODO: pagination
+#TODO: formatting
 
 def listNews(request):
-    # newsapi = NewsApiClient(api_key='cb682c4048944cd1a9f17e88bb3ad67f')
+    results = []
+
+    reddit = praw.Reddit(client_id="4ZQq_6IHGR6t6A",
+                        client_secret="AmKTDcjfrcxU6yjpSIu4uY7DvCU",
+                        user_agent="testscript by /u/fornewsapi")
+
+    for submission in reddit.subreddit("news").top(limit=10):
+        results.append({
+            'headline': submission.title,
+            'link': submission.url,
+            'source': 'reddit'
+        })
+
+    newsapi = NewsApiClient(api_key='cb682c4048944cd1a9f17e88bb3ad67f')
+
+    us_articles = newsapi.get_top_headlines(category="general", page_size=10)
+
+    for news in us_articles['articles']:
+        results.append({
+            'headline': news['title'],
+            'link': news['url'],
+            'source': 'newsAPI'
+        })
 
     # results = {}
 
@@ -22,11 +47,19 @@ def listNews(request):
     #     'results': results
     # }
 
-    return render(request, 'news/index.html')
+    # return render(request, 'news/index.html')
+    return JsonResponse(results, safe=False)
 
 
 def searchNews(request):
     if request.method == 'POST':
+        reddit = praw.Reddit(client_id="4ZQq_6IHGR6t6A",
+                            client_secret="AmKTDcjfrcxU6yjpSIu4uY7DvCU",
+                            user_agent="testscript by /u/fornewsapi")
+
+        for submission in reddit.subreddit("news").hot(limit=10):
+            print(submission.title)
+
         newsapi = NewsApiClient(api_key='cb682c4048944cd1a9f17e88bb3ad67f')
 
         keyword = request.POST['kw']
